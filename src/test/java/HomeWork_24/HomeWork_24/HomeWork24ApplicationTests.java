741e4a9c522211a350b6b8259a468b449707625f
Dolgorykij <dolgo-rykij@mail.ru>
2024-10-08 00:35:27 +0300

    init

---
+++ src/test/java/HomeWork_24/HomeWork_24/HomeWork24ApplicationTests.java
@@ -0,0 +1,13 @@
+package HomeWork_24.HomeWork_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
